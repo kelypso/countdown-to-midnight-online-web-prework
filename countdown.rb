b077c58af4d4d9e248b32dc6e40741a41d1066ca
Kelsey Low <kklowlow@gmail.com>
2018-12-24 05:50:26 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,12 +7,3 @@ def countdown(time)
   end
   return "HAPPY NEW YEAR!"
 end
-
-
-def countdown(seconds)
-  while seconds > 0
-    puts "#{seconds} SECOND(S)!"
-    seconds -= 1
-  end
-  return "HAPPY NEW YEAR!"
-end
